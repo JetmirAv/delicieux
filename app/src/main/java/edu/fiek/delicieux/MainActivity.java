bb4c18444f8de5965c401b97d304d03db31e2d94
Migena Likaj <migena.likaj@studentet.uni-pr.edu>
2020-06-30 22:00:43 +0200

    change locale

--- app/src/main/java/edu/fiek/delicieux/MainActivity.java
+++ app/src/main/java/edu/fiek/delicieux/MainActivity.java
@@ -5,14 +5,22 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import edu.fiek.delicieux.helpers.LocaleManager;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "App";
+    public static LocaleManager localeManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +38,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp() {
         return super.onSupportNavigateUp();
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        localeManager = new LocaleManager(base);
+        super.attachBaseContext(localeManager.setLocale(base));
+        Log.d(TAG, "attachBaseContext");
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        localeManager.setLocale(this);
+        Log.d(TAG, "onConfigurationChanged: " + newConfig.locale.getLanguage());
     }
 }
--- app/src/main/java/edu/fiek/delicieux/ui/profile/ChangeLanguageFragment.java
+++ app/src/main/java/edu/fiek/delicieux/ui/profile/ChangeLanguageFragment.java
@@ -1,46 +1,95 @@
 package edu.fiek.delicieux.ui.profile;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 
+import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
+import edu.fiek.delicieux.MainActivity;
 import edu.fiek.delicieux.R;
 
 
 public class ChangeLanguageFragment extends Fragment {
 
-    private static final String TAG = "MainActivity";
+    ImageView btnBack;
+    TextView albtxt;
+    TextView engtxt;
+    View view;
 
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.fragment_change_language);
-//
-//        ArrayList<String> names = new ArrayList<>();
-//        names.add("Albanian");
-//        names.add("Albanian");
-//        names.add("Albanian");
-//        names.add("Albanian");
-//        names.add("Albanian");
-//        names.add("Albanian");
-//
-//
-//    }
-//
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.fragment_change_language, container, false);
+        view = inflater.inflate(R.layout.fragment_change_language, container, false);
+
+        btnBack = view.findViewById(R.id.backbtn);
+        albtxt = view.findViewById(R.id.albId);
+        engtxt = view.findViewById(R.id.engId);
+
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Navigation.findNavController(v).popBackStack();
+            }
+        });
+        albtxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setNewLocale("al");
+
+            }
+        });
+
+        engtxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setNewLocale("en");
+
+            }
+        });
         return view;
+    }
+
+    public void setLocale(String lang) {
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.setLocale(new Locale(lang.toLowerCase()));
+        res.updateConfiguration(conf, dm);
+    }
+
+
+    private boolean setNewLocale(String language) {
+        MainActivity.localeManager.setNewLocale(getContext(), language);
+
+        Intent i = new Intent(getContext(), MainActivity.class);
+        startActivity(i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
+
+        Toast.makeText(getContext(), "Language changed successfully", Toast.LENGTH_SHORT).show();
+        return true;
     }
 
 }

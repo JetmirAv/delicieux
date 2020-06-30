4789dbd3ed6acbc34dee20faea68bc7c1738470a
Jetmir Avdullahu <jetmir.avdullahu@studentet.uni-pr.edu>
2020-06-30 23:36:03 +0200

    cookbook and receipt detailview

--- app/src/main/java/edu/fiek/delicieux/adapter/RecipesAdapter.java
+++ app/src/main/java/edu/fiek/delicieux/adapter/RecipesAdapter.java
@@ -20,15 +20,18 @@ import java.util.List;
 
 import edu.fiek.delicieux.R;
 import edu.fiek.delicieux.models.RecipesFood;
+import edu.fiek.delicieux.ui.recipes.RecipesDetailViewModel;
 
 
 public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.RecipesViewHolder> {
     Context context;
     List<RecipesFood> recipesFoodList;
+    RecipesDetailViewModel recipesDetailViewModel;
 
-    public RecipesAdapter(Context context, List<RecipesFood> recipesFoodList) {
+    public RecipesAdapter(Context context, List<RecipesFood> recipesFoodList, RecipesDetailViewModel recipesDetailViewModel) {
         this.context = context;
         this.recipesFoodList = recipesFoodList;
+        this.recipesDetailViewModel = recipesDetailViewModel;
     }
 
     @NonNull
@@ -38,20 +41,22 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.RecipesV
         View view = LayoutInflater.from(context).inflate(R.layout.recipes_food_row_item, parent, false);
 
         Button showMoreBtn = view.findViewById(R.id.show_more);
-        showMoreBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(v).navigate(R.id.recipesDetailFragment);
-            }
-        });
+
 
         return new RecipesViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(RecipesViewHolder holder, int position) {
+    public void onBindViewHolder(RecipesViewHolder holder, final int position) {
         holder.name.setText(recipesFoodList.get(position).getTitle());
         Glide.with(holder.foodImage.getContext()).load(recipesFoodList.get(position).getMedia()).into(holder.foodImage);
+        holder.showMoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recipesDetailViewModel.setSelected(recipesFoodList.get(position));
+                Navigation.findNavController(v).navigate(R.id.recipesDetailFragment);
+            }
+        });
     }
 
     @Override
@@ -63,10 +68,13 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.RecipesV
     public static final class RecipesViewHolder extends RecyclerView.ViewHolder {
         ImageView foodImage;
         TextView name;
+        Button showMoreBtn;
+
         public RecipesViewHolder(@NonNull View itemView) {
             super(itemView);
             foodImage = itemView.findViewById(R.id.food_image);
             name = itemView.findViewById(R.id.name);
+            showMoreBtn = itemView.findViewById(R.id.show_more);
         }
     }
 }

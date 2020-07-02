e50ac21dfec0812d047bc6c594531ba9696202e7
Jetmir Avdullahu <jetmir.avdullahu@studentet.uni-pr.edu>
2020-07-02 22:54:23 +0200

    added maps to cookbook detail view and added dummy data for favourites

--- app/src/main/java/edu/fiek/delicieux/ui/favourite/FavouriteCookbooksFragment.java
+++ app/src/main/java/edu/fiek/delicieux/ui/favourite/FavouriteCookbooksFragment.java
@@ -9,19 +9,25 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 import edu.fiek.delicieux.R;
+import edu.fiek.delicieux.adapter.CookBookAdapter;
 import edu.fiek.delicieux.models.CookBook;
+import edu.fiek.delicieux.models.RecipesFood;
+import edu.fiek.delicieux.ui.cookbook.CookBookDetailViewModel;
+import edu.fiek.delicieux.ui.recipes.RecipesDetailViewModel;
 
 public class FavouriteCookbooksFragment extends Fragment {
     public static final String ARG_OBJECT = "object";
 
     RecyclerView recyclerView;
-    FavouriteCookbooksAdapter favouriteCookbooksAdapter;
+    CookBookDetailViewModel viewModel;
+    CookBookAdapter favouriteCookbooksAdapter;
     View view;
 
     @Nullable
@@ -38,19 +44,25 @@ public class FavouriteCookbooksFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         Bundle args = getArguments();
 
-        ArrayList<CookBook> cookBooks = new ArrayList<>();
-//        cookBooks.add(new CookBook("Test", 21));
-//        cookBooks.add(new CookBook("Test", 21));
-//        cookBooks.add(new CookBook("Test", 21));
-//        cookBooks.add(new CookBook("Test", 21));
-//        cookBooks.add(new CookBook("Test", 21));
-//        cookBooks.add(new CookBook("Test", 21));
-//        cookBooks.add(new CookBook("Test", 21));
+        viewModel = new ViewModelProvider(requireActivity()).get(CookBookDetailViewModel.class);
 
+
+        ArrayList<CookBook> cookBooks = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
+        list.add("-MB5SVmNvcJd6O1N4BGl");
+        list.add("-MB5SVmNvcJd6O1N4BGl");
+        list.add("-MB5SVmNvcJd6O1N4BGl");
+
+
+        for (int i = 0; i < 5; i++) {
+            CookBook cookBook = new CookBook("Test", "21", "Test", list, "-MB5TjKFjCjTi18AoRlD");
+            cookBook.setMedia("https://firebasestorage.googleapis.com/v0/b/delicieux-5b867.appspot.com/o/media%2Fasiafood1.png?alt=media&token=50c585b6-1d9c-413b-a4ed-4306bc2c2e82");
+            cookBooks.add(cookBook);
+        }
 
         recyclerView = view.findViewById(R.id.favourite_cookbook_recycler_view);
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
-        favouriteCookbooksAdapter = new FavouriteCookbooksAdapter(getContext(), cookBooks);
+        favouriteCookbooksAdapter = new CookBookAdapter(getContext(), cookBooks, viewModel);
         recyclerView.setAdapter(favouriteCookbooksAdapter);
 
 
